20802ee6c09e3f9c3f3dfe2e79c4d97d458c6788
Maaya <yamauchimaaya@gmail.com>
2022-11-21 12:59:08 -0600

    initial setup with npm modules

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const cors = require('cors');
+
+const PORT = 3000;
+const express = require('express');
+const server = express();
+const morgan = require('morgan');
+server.use(morgan('dev'));
+server.use(cors())
+server.use(express.json())
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
+const { client } = require('./db');
+client.connect();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
