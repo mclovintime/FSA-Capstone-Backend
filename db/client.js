24bb3bb13d6e209e21dd9ff84567beb3c2f34e0a
Thomas McLaughlin <123tommygun1@gmail.com>
2022-11-30 15:00:01 -0500

    fix render

--- db/client.js
+++ db/client.js
@@ -2,10 +2,10 @@ const { Client } = require("pg");
 // const client = new Client("postgres://localhost:5432/capstone");
 
 
-const client = new Client({port: process.env.DATABASE_URL
-  || "postgres://localhost:5432/capstone", 
-ssl: process.env.NODE_ENV ==="production" ? {rejectUnauthorized: false} : undefined
-});
+const client = new Client(process.env.DATABASE_URL
+  || "postgres://localhost:5432/capstone"
+
+);
 
 
 module.exports = client
