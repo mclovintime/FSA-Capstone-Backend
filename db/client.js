c4b9ca421b1a014d2dcc0f3ed4511d51bf93c924
Maaya <yamauchimaaya@gmail.com>
2023-06-21 21:36:04 -0500

    updated dbb

--- db/client.js
+++ db/client.js
@@ -2,7 +2,7 @@ const { Client } = require("pg");
 
 const client = new Client({
   connectionString:
-    process.env.DATABASE_URL || "postgres://localhost:5432/capstone",
+    process.env.DATABASE_URL || "postgres://localhost:5432/capstone-backend",
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
