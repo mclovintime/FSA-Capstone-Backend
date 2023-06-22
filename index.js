72eff5c7ff40fd74c5e417c662526188a5c595a3
Maaya <yamauchimaaya@gmail.com>
2023-06-21 21:53:03 -0500

    updated client

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-require('dotenv').config();
-const cors = require('cors');
-const stripe = require('stripe')('sk_test_51M6yNAChZbepu0tegirpoYXAh8qTu81xCEJQJuKVPkuHeSAVBH0NWxx0juJZNuFNdAtNYTl8QRTgyThIh1ABqwUt00IWs0OqDY')
-const uuid = require('uuid')
+require("dotenv").config();
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51M6yNAChZbepu0tegirpoYXAh8qTu81xCEJQJuKVPkuHeSAVBH0NWxx0juJZNuFNdAtNYTl8QRTgyThIh1ABqwUt00IWs0OqDY"
+);
+const uuid = require("uuid");
 
 // const PORT = 3000;
 // const PORT = process.env.PORT || 3000;
-const{PORT=8080}=process.env
+const { PORT = 8080 } = process.env;
 
-const express = require('express');
+const express = require("express");
 const server = express();
-const morgan = require('morgan');
-server.use(morgan('dev'));
-server.use(cors())
-server.use(express.json())
+const morgan = require("morgan");
+server.use(morgan("dev"));
+server.use(cors());
+server.use(express.json());
 
-const apiRouter = require('./api');
-server.use('/api', apiRouter);
+const apiRouter = require("./api");
 
-const {client} = require('./db/client');
+const { client } = require("./db/client");
 client.connect();
 
+server.get("/", (req, res) => {
+  res.json({
+    message: "You have connected to the API Server",
+  });
+});
 
+server.use((req, res, next) => {
+  console.log("<___Body Logger START____>");
+  console.log(req.method);
+  console.log("____Body Logger END____>");
+  next();
+});
 
+server.use("/api", apiRouter);
 
 server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
+  console.log("The server is up on port", PORT);
 });
